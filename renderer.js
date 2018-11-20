231fba22b1785e67d85c9b3f693749c2324cac82
Jerry Chou <imjatsz@gmail.com>
2018-11-20 21:22:18 +0800

    add context menu for input fields

--- renderer.js
+++ renderer.js
@@ -1,3 +1,22 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+// In the renderer process:
+var remote = require('electron').remote;
+// `remote.require` since `Menu` is a main-process module.
+var buildEditorContextMenu = remote.require('electron-editor-context-menu');
+
+window.addEventListener('contextmenu', function(e) {
+  // Only show the context menu in text editors.
+  if (!e.target.closest('textarea, input, [contenteditable="true"]')) return;
+
+  var menu = buildEditorContextMenu();
+
+  // The 'contextmenu' event is emitted after 'selectionchange' has fired but possibly before the
+  // visible selection has changed. Try to wait to show the menu until after that, otherwise the
+  // visible selection will update after the menu dismisses and look weird.
+  setTimeout(function() {
+    menu.popup(remote.getCurrentWindow());
+  }, 30);
+});
